cb9a18452a42433b470bc9fbed30d37cbade9b59
Verolian <verolianopiyo@gmail.com>
2023-12-14 23:43:10 +0300

    Geometry module

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,14 @@
+class BaseGeometry:
+    pass
+
+# Instantiate the object
+bg = BaseGeometry()
+
+# Print the object
+print(bg)
+
+# Print the attributes of the object
+print(dir(bg))
+
+# Print the attributes of the class
+print(dir(BaseGeometry))
