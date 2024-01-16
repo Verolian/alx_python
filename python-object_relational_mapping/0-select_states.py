b651a87e4de9cc04c0d8432d484f00c17becd664
Verolian <verolianopiyo@gmail.com>
2024-01-16 05:17:56 +0300

    Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit(1)
+
+    # Extract command-line arguments
+    username, password, database = argv[1], argv[2], argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a MySQL cursor
+    cursor = db.cursor()
+
+    # Execute the SQL query to get all states and sort by states.id
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
