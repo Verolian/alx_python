ff494a13b55ddabafc5442f53dd7f940cf3911d7
Verolian <verolianopiyo@gmail.com>
2024-01-17 05:29:33 +0300

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python3
-"""
-Start link class to table in database
-"""
-import sys
-from model_state import Base, State
+"""Defines the State class and Base instance."""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
+# Create an instance of the Base class
+Base = declarative_base()
 
+class State(Base):
+    """State class."""
+    __tablename__ = 'states'  # Link to the MySQL table 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+# Replace 'your_username', 'your_password', and 'hbtn_0e_0_usa' with your actual MySQL credentials
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    from sys import argv
+
+    # Check if the correct number of arguments is provided
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(argv[0]))
+        exit(1)
+
+    # Extract command-line arguments
+    username, password, database = argv[1], argv[2], argv[3]
+
+    # Create an engine to connect to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database), pool_pre_ping=True)
+
+    # Create all tables in the database
     Base.metadata.create_all(engine)
