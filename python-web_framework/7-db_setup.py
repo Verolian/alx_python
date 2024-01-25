088b0f0c99650e80d111a0e282f8942277937304
Verolian <verolianopiyo@gmail.com>
2024-01-25 23:23:33 +0300

    Flask and SQLAlchemy Integration

--- python-web_framework/7-db_setup.py
+++ python-web_framework/7-db_setup.py
@@ -1,32 +1,48 @@
-#!/usr/bin/python3
-"""
-Starting my first Flask web application
-"""
-from models import storage
-from models.state import State
-from os import environ
-from flask import Flask, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import re
+import sys
+
+# Check for command-line arguments
+if len(sys.argv) != 4:
+    print("Usage: python 8-add_retrieve_users.py <db_username> <db_password> <db_name>")
+    sys.exit(1)
+
+db_username = sys.argv[1]
+db_password = sys.argv[2]
+db_name = sys.argv[3]
+db_host = 'localhost'
+
 app = Flask(__name__)
 
+########## TO DO 1 #######
+# Add your code to connect to the database here
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_username}:{db_password}@{db_host}/{db_name}'
+db = SQLAlchemy(app)
+###############################################################
 
-@app.teardown_appcontext
-def close_db(error):
-    """
-    Remove the SQLAlchemy session
-    """
-    storage.close()
+############################ TO DO 2 ##############################
+# Define your USER Model class here
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+#############################################################
+
+# Create the database tables
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+create_tables()  # This calls the function to create tables
 
 
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """
-    A route that displays cities in our database in html
-    """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    return render_template('7-states_list.html', states=states)
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello, ALX Flask!"
 
-
-if __name__ == "__main__":
-    """ Main Function """
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
